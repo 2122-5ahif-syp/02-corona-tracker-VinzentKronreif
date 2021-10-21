4f97540b1acd12141c314e010f740ad578d3b5e8
VinzentKronreif <vini.kro@gmail.com>
2021-10-21 14:08:34 +0200

    added Visitor Class and Properties

--- src/main/java/at/htl/Visitor.java
+++ src/main/java/at/htl/Visitor.java
@@ -1,4 +1,20 @@
 package at.htl;
 
+import javax.persistence.*;
+import java.time.Instant;
+
+@Entity
+@Table(name = "VISITOR")
 public class Visitor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    String firstName;
+    String lastName;
+    String email;
+    int telephoneNo;
+
+    Instant timestamp;
+
 }
